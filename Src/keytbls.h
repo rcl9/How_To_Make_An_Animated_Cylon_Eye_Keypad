8b9514a75b250b6c3320b2a65ddb0afce6eb0364
RRCA - Rob's 70's & 80's Retro Computing Archive <RetroComputingArchive@gmail.com>
2026-08-29 02:54:53 -0400

    Initial commit

---
+++ Src/keytbls.h
@@ -0,0 +1,30 @@
+/*
+			Keypad Encoding Equates
+		Copyright (c) 1987 By Robert Lansdale
+
+NOTE: a comma can be returned from the keypad by pressing the Shift up,
+or shift down keys with the YES or NO keys.
+*/
+
+/* ------------------>>>>  Key Value Definitions  <<<<------------------- */
+
+#define		CANCEL		0x03		/* cancel key (control C) */
+#define		ENTER		0x0d		/* enter key		*/
+#define		F1		0x080		/* function key # 1	*/
+#define		F2		0x081		/* function key # 2	*/
+#define		F3		0x082		/* function key # 3	*/
+#define		F4		0x083		/* function key # 4	*/
+#define		F5		0x084		/* function key # 5	*/
+#define		F6		0x085		/* function key # 6	*/
+#define		F7		0x086		/* function key # 7	*/
+#define		F8		0x087		/* function key # 8	*/
+#define		F9		0x088		/* function key # 9	*/
+#define		F10		0x089		/* function key # 10	*/
+#define		F11		0x08a		/* function key # 11	*/
+#define		F12		0x08b		/* function key # 12	*/
+#define		YES_LEFT_CURS	0x8		/* yes, or left_cursor (back space) */
+#define		NO_RIGHT_CURS	0x08c		/* no, or right_cursor	*/
+#define		UP_CURS		0x08d		/* up cursor key	*/
+#define		DOWN_CURS	0x08e		/* down cursor key	*/
+
+
